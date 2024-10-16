1e7af5876a97b73ac07f7772cc616220237d6818
Aezakmi <andrearamadhan123@gmail.com>
2024-10-16 11:02:09 +0700

    add

--- Assets/Scripts/ForFun Script/LegStepper.cs
+++ Assets/Scripts/ForFun Script/LegStepper.cs
@@ -11,6 +11,8 @@ public class LegStepper : MonoBehaviour
     private float _wantStepAtDistance;    
     [SerializeField]
     private float _moveDuration;
+    [SerializeField]
+    float _stopOvershootFraction;
 
     public bool Moving;
 
@@ -43,7 +45,49 @@ public class LegStepper : MonoBehaviour
 
     }
 
-    private void Update()
+    IEnumerator Move() 
+    {
+        Moving = true;
+
+        Vector3 startPoint = transform.position;
+        Quaternion startRot = transform.rotation;
+
+        Quaternion endRot = _homeTransform.rotation;
+
+        Vector3 towardHome = (_homeTransform.position - transform.position);
+
+        float overshootDistance = _wantStepAtDistance * _wantStepAtDistance;
+        Vector3 overshootVector = towardHome * overshootDistance;
+
+        overshootVector = Vector3.ProjectOnPlane(overshootVector, Vector3.up);
+
+        Vector3 endPoint = _homeTransform.position + overshootVector;
+
+        Vector3 centerPoint = (startPoint +endPoint);
+
+        centerPoint += _homeTransform.up * Vector3.Distance(startPoint, endPoint) / 2f;
+
+        float timeElapsed = 0;
+        do
+        {
+            timeElapsed += Time.deltaTime;
+            float normalizedTime = timeElapsed / _moveDuration;
+
+            transform.position =
+                Vector3.Lerp(
+                    Vector3.Lerp(startPoint, centerPoint, normalizedTime),
+                    Vector3.Lerp(centerPoint, endPoint, normalizedTime),
+                    normalizedTime);
+
+            transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
+            yield return null;
+        }
+        while (timeElapsed < _moveDuration);
+
+        Moving = false;
+    }
+
+    public void TryMove()
     {
         if (Moving) return;
 
@@ -51,7 +95,7 @@ public class LegStepper : MonoBehaviour
 
         if (distFromHome > _wantStepAtDistance)
         {
-            StartCoroutine(MoveToHome());
+            StartCoroutine(Move());
         }
     }
 }
--- Assets/Scripts/ForFun Script/MonsterScript.cs
+++ Assets/Scripts/ForFun Script/MonsterScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MonsterScript : MonoBehaviour
 {
@@ -12,12 +13,49 @@ public class MonsterScript : MonoBehaviour
     Transform _headBone;
 
     [SerializeField]
+    float _turnSpeed;
+    [SerializeField]
+    float _moveSpeed;
+
+    [SerializeField]
+    float turnAcceleration;
+    [SerializeField]
+    float moveAcceleratiaon;
+
+    [SerializeField]
+    float minDistToTarget;
+    [SerializeField]
+    float maxDistToTarget;
+    
+    [SerializeField]
+    float maxAngleToTarget;
+
+    Vector3 currentVelocity;
+
+    float currentAngularVelocity;
+
+    [SerializeField]
+    LegStepper _frontLeftLegStepper;
+    [SerializeField]
+    LegStepper _frontRightLegStepper;
+    [SerializeField]
+    LegStepper _BackLeftLegStepper;
+    [SerializeField]
+    LegStepper _BackRightLegStepper;
+
+    [SerializeField]
     float _headMaxTurnAngle;
     [SerializeField]
     float _headTracingSpeed;
 
+
+    private void Awake()
+    {
+        StartCoroutine(LegUpdatCoroutine());
+    }
     private void LateUpdate()
     {
+        RootMotionUpdate();
         HeadTracing();
     }
 
@@ -45,5 +83,87 @@ public class MonsterScript : MonoBehaviour
             1 - Mathf.Exp(-_headTracingSpeed * Time.deltaTime)
             );
 
+    }
+
+    void RootMotionUpdate()
+    {
+        Vector3 towardTarget = _target.position - transform.position;
+
+        Vector3 towardTargetProjected = Vector3.ProjectOnPlane(towardTarget, transform.up);
+
+        float angToTarget = Vector3.SignedAngle(transform.forward, towardTargetProjected, transform.up);
+
+        float targetAngularVelocity = 0;
+
+        if (Mathf.Abs(angToTarget) > maxAngleToTarget)
+        {
+
+            if (angToTarget > 0) 
+            {
+                targetAngularVelocity = _turnSpeed;
+            }
+
+            else
+            {
+                targetAngularVelocity = -_turnSpeed;
+            }
+
+            currentAngularVelocity = Mathf.Lerp(
+                currentAngularVelocity,
+                targetAngularVelocity,
+                1 - Mathf.Exp(-turnAcceleration * Time.deltaTime)
+                );
+
+        }
+
+        transform.Rotate(0, Time.deltaTime * -currentAngularVelocity, 0, Space.World);
+
+
+        Vector3 targetVelocity = Vector3.zero;
+
+        if (Mathf.Abs(angToTarget) > 90)
+        {
+            float distToTarget = Vector3.Distance(transform.position, _target.position);
+
+            if (distToTarget > maxDistToTarget)
+            {
+                targetVelocity = _moveSpeed * towardTargetProjected.normalized;
+            }
+
+            else if (distToTarget < minDistToTarget)
+            {
+                targetVelocity = _moveSpeed * -towardTargetProjected.normalized;
+            }
+        }
+
+        currentVelocity = Vector3.Lerp(
+            currentVelocity,
+            targetVelocity,
+            1 - Mathf.Exp(-moveAcceleratiaon * Time.deltaTime)
+            );
+
+        transform.position += currentVelocity * Time.deltaTime;
+    }
+
+    IEnumerator LegUpdatCoroutine()
+    {
+
+        while (true)
+        {
+
+            do
+            {
+                _frontLeftLegStepper.TryMove();
+                _BackRightLegStepper.TryMove();
+                yield return null;
+            } while (_BackRightLegStepper.Moving || _frontLeftLegStepper.Moving);
+
+            do
+            {
+                _frontRightLegStepper.TryMove();
+                _BackLeftLegStepper.TryMove();
+                yield return null;
+            } while (_BackLeftLegStepper.Moving || _frontRightLegStepper.Moving);
+        }
     }
 }
