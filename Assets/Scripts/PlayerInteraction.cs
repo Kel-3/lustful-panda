03477980cbbd94512d8be97439023b51bacc8a8f
RizqiJagad <taqiyrizqijagad@gmail.com>
2024-10-17 19:02:50 +0700

    SystemQuest

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float interactionRange = 2.0f;  
+    public Transform interactableObject;  
+    private bool hasGivenQuest = false;    
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            CheckForInteraction();
+        }
+    }
+
+    void CheckForInteraction()
+    {
+        
+        float distance = Vector3.Distance(transform.position, interactableObject.position);
+
+        if (distance <= interactionRange && !hasGivenQuest)
+        {
+            InteractWithObject();  
+        }
+        else if (distance > interactionRange)
+        {
+            Debug.Log("Object tidak terdeteksi");
+        }
+    }
+
+    void InteractWithObject()
+    {
+        Debug.Log("Interaksi dengan object");
+        GiveQuest();
+    }
+
+    void GiveQuest()
+    {
+        if (!hasGivenQuest)
+        {
+            
+            Quest newQuest = new Quest("Temukan Kunci", "Find and collect the mysterious key.", "Kunci");
+            QuestManager.instance.AddQuest(newQuest);
+            hasGivenQuest = true;  
+        }
+        else
+        {
+            Debug.Log("Tidak ada Quest");
+        }
+    }
+}
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public static QuestManager instance;  
+
+    private List<Quest> activeQuests = new List<Quest>();  
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+    public void AddQuest(Quest newQuest)
+    {
+        activeQuests.Add(newQuest);
+        Debug.Log("Quest Added: " + newQuest.questName);
+    }
+
+    
+    public void ItemCollected(string itemName)
+    {
+        foreach (Quest quest in activeQuests)
+        {
+            if (quest.targetItem == itemName && !quest.isCompleted)
+            {
+                quest.CompleteQuest();
+                Debug.Log("Quest Completed: " + "Temukan Pintu Untuk Membukanya");
+            }
+        }
+    }
+}
