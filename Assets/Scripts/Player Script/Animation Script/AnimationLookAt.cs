e0f91d4c8728b5d9a0d799ea1efde0bbeda30ad6
Myrth <74478820+ratihtresnati@users.noreply.github.com>
2024-10-22 13:15:12 +0700

    mengganti animation look at

--- Assets/Scripts/Player Script/Animation Script/AnimationLookAt.cs
+++ Assets/Scripts/Player Script/Animation Script/AnimationLookAt.cs
@@ -5,171 +5,110 @@ using UnityEngine.Animations.Rigging;
 
 public class AnimationLookAt : MonoBehaviour
 {
-    [SerializeField] private Animator _animator;
-    [SerializeField] public Rig rigWeight ;
-
-    [Header("Turn Animation")]
-    public Transform lookPoint, rightPoint, leftPoint, Character;
-    public float duration = 1f;
-    private float elapsedTime = 0f;
-    private Vector3 startPosition;
-    private Vector3 startRotation = new Vector3(0, 0, 0);
-    private Vector3 newPosition;
-    private Vector3 anim_rotation;
-    private Vector3 rightPosition, leftPosition;
+    public float duration = 0.3f;
+    public Rig rightTurn;
+    public Rig leftTurn;
     private Turn _currentState = Turn.Idle;
-    private float positionTolerance = 0.1f;
-    private float t;
-
-    [SerializeField] private bool isRotate = false;
-
-    private void Start()
-    {
-        startPosition = lookPoint.transform.localPosition; 
-        rightPosition = rightPoint.transform.localPosition;
-        leftPosition = leftPoint.transform.localPosition;
-
-        // rigWeight.weight = 0;
-    }
 
     private void Update()
     {
-        float yRotation = Character.eulerAngles.y;
+        float yRotation = transform.eulerAngles.y;
 
-        Debug.Log(yRotation);
+        if(yRotation == 270||yRotation == 90||yRotation == 180|| yRotation == 0)
+        {
+            _currentState = Turn.StopTurn;
+        }
+
+        if(yRotation == 45 || yRotation == 135 || yRotation == 315 || yRotation == 225)
+        {
+            _currentState = Turn.StopTurn;
+        }  
 
         switch (_currentState)
         {
             case Turn.Idle:
-
-                if(isRotate == false)
+                if(yRotation >= 25f && yRotation <= 65f || yRotation <= 330f && yRotation >= 300f)
                 {
-                    if(yRotation >= 25f && yRotation <= 65f || yRotation <= 330f && yRotation >= 300f)
+                    if(Input.GetKey("d"))
                     {
-                        if(Input.GetKey("d"))
-                        {
-                            _currentState = Turn.Right;
-                        }
-
-                        if(Input.GetKey("a"))
-                        {
-                            _currentState = Turn.Left;
-                        }
+                        _currentState = Turn.Right;
                     }
 
-                    if( yRotation >= 30f && yRotation <= 60f || yRotation <= 150f && yRotation >= 120f)
+                    if(Input.GetKey("a"))
                     {
-                        if(Input.GetKey("w"))
-                        {
-                            _currentState = Turn.Left;
-                        }
-
-                        if(Input.GetKey("s"))
-                        {
-                            _currentState = Turn.Right;
-                        }
-                    }
-
-                    if(yRotation <= 150f && yRotation >= 120f || yRotation >= 210f && yRotation <= 240f)
-                    {
-                        if(Input.GetKey("d"))
-                        {
-                            _currentState = Turn.Left;
-                        }
-
-                        if(Input.GetKey("a"))
-                        {
-                            _currentState = Turn.Right;
-                        }
-                    }
-
-                    if(yRotation <= 330f && yRotation >= 300f || yRotation >= 210f && yRotation <= 240f)
-                    {
-                        if(Input.GetKey("w"))
-                        {
-                            _currentState = Turn.Right;
-                        }
-
-                        if(Input.GetKey("s"))
-                        {
-                            _currentState = Turn.Left;
-                        }
+                        _currentState = Turn.Left;
                     }
                 }
-                
 
-                if(Input.GetKey(KeyCode.Q))
+                if( yRotation >= 30f && yRotation <= 60f || yRotation <= 150f && yRotation >= 120f)
                 {
-                    rigWeight.weight = 0;
-                }
-                else
-                {
-                    rigWeight.weight = 1;
-                }
+                    if(Input.GetKey("w"))
+                    {
+                        _currentState = Turn.Left;
+                    }
 
-                if(Input.GetKey(KeyCode.Space))
-                {
-                    rigWeight.weight = 0;
-                }
-                else
-                {
-                    rigWeight.weight = 1;
+                    if(Input.GetKey("s"))
+                    {
+                        _currentState = Turn.Right;
+                    }
                 }
 
+                if(yRotation <= 150f && yRotation >= 120f || yRotation >= 210f && yRotation <= 240f)
+                {
+                    if(Input.GetKey("d"))
+                    {
+                        _currentState = Turn.Left;
+                    }
+
+                    if(Input.GetKey("a"))
+                    {
+                        _currentState = Turn.Right;
+                    }
+                }
+
+                if(yRotation <= 330f && yRotation >= 300f || yRotation >= 210f && yRotation <= 240f)
+                {
+                    if(Input.GetKey("w"))
+                    {
+                        _currentState = Turn.Right;
+                    }
+
+                    if(Input.GetKey("s"))
+                    {
+                        _currentState = Turn.Left;
+                    }
+                }
                 break;
             case Turn.Right:
-                        
-                elapsedTime += Time.fixedDeltaTime;
-                t = Mathf.Clamp01(elapsedTime / duration);
-
-                newPosition = Vector3.Lerp(startPosition, rightPosition, t);
-
-                transform.localPosition = newPosition;
-                if(Vector3.Distance(transform.localPosition, rightPosition) < positionTolerance)
-                {
-                    _currentState = Turn.StopTurn;
-                    StopTurn();
-                }
+                rightTurn.weight += Time.deltaTime * duration;
                 break;
             case Turn.Left:
-                elapsedTime += Time.fixedDeltaTime;
-                t = Mathf.Clamp01(elapsedTime / duration); 
-            
-                newPosition = Vector3.Lerp(startPosition, leftPosition, t);
-
-                transform.localPosition = newPosition;
-
-                if(Vector3.Distance(transform.localPosition, leftPosition) < positionTolerance)
-                {
-                    _currentState = Turn.StopTurn;
-                    StopTurn();
-                }
+                leftTurn.weight += Time.deltaTime * duration;     
                 break;
             case Turn.StopTurn:
-                elapsedTime += Time.deltaTime;
-                t = Mathf.Clamp01(elapsedTime / duration); 
-            
-                newPosition = Vector3.Lerp(newPosition, startPosition, t);
-
-                transform.localPosition = newPosition;
-
-                if(Vector3.Distance(transform.localPosition, startPosition) < positionTolerance)
-                {
-                    _currentState = Turn.Idle;
-                    StopTurn();
-                }
+                ResetWeight();
                 break;
-        }  
-        Debug.Log(_currentState);
+        }
     }
 
-    public void StopTurn()
+    private void ResetWeight()
     {
-        elapsedTime = 0;
-        isRotate = false;
-    }
+        rightTurn.weight -= Time.deltaTime * duration;
 
+        if(rightTurn.weight == 0)
+        {
+            _currentState = Turn.Idle;
+        }
+
+        leftTurn.weight -= Time.deltaTime * duration;
+                    
+        if(leftTurn.weight == 0)
+        {
+            _currentState = Turn.Idle;
+        }
+    }
 }
+    
 public enum Turn
     {
         Idle,
